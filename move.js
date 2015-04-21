7f91a48248c8ae034332fab829fe672b5b5cc5a6
Astro <astro@spaceboyz.net>
2015-04-22 00:17:06 +0200

    manual files sorting order

--- move.js
+++ move.js
@@ -138,7 +138,14 @@ function go() {
                     return next();
                 }
 
-                files.forEach(function(file) {
+                files.sort(function(a, b) {
+                    if (a > b)
+                        return -1;
+                    else if (a < b)
+                        return 1;
+                    else
+                        return 0;
+                }).forEach(function(file) {
                     if (!/^\./.test(file)) {
                         pendingPaths.push(path + "/" + file);
                     }
